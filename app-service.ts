92362154f944cc4da2b9357f97e8cd7abacb713f
swetha0 <gaali.swetha@gmail.com>
2020-06-08 19:16:04 -0400

    FSJ0420A104

--- app-service.ts
+++ app-service.ts
@@ -1,7 +1,7 @@
 export interface ArrayService {
-  arrayMultiply(myArray : any) : Array<Number>;
-  arraySeparate(myArray : any) : Array<string>;
-  arraySplit(myString : string) : Array<number>;
-  arraySort(myArray : any) : Array<string>;
-  arrayReplace(myArray : any) : Array<Number>;
+  arrayMultiply(myArray: any): Array<Number>;
+  arraySeparate(myArray: any): Array<string>;
+  arraySplit(myString: string): Array<number>;
+  arraySort(myArray: any): Array<string>;
+  arrayReplace(myArray: any): Array<Number>;
 }
